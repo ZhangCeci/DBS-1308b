800c6ddf3b5368fadc0333b866ae1225325b8ca8
ZhangCeci <111175422+ZhangCeci@users.noreply.github.com>
2022-08-13 17:10:19 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,147 +1,62 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "9246f938",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from flask import Flask,render_template, request"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "31cba41a",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import joblib"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "id": "74832ccc",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "app= Flask(__name__)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 4,
-   "id": "77f0163e",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "@app.route(\"/\",methods=[\"GET\",\"POST\"])\n",
-    "def index():\n",
-    "    if request.method == \"POST\":\n",
-    "        rates=float(request.form.get(\"rates\"))\n",
-    "        print(rates)\n",
-    "        model1=joblib.load(\"regression_DBS\")\n",
-    "        pred1=model1.predict([[rates]])\n",
-    "        model2=joblib.load(\"tree_DBS\")\n",
-    "        pred2=model2.predict([[rates]])\n",
-    "        return(render_template(\"index.html\",result1=pred1,result2=pred2))\n",
-    "    else:\n",
-    "        return(render_template(\"index.html\",result1=\"waiting\",result2=\"waiting\"))  "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "6bb8d8d7",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " * Serving Flask app \"__main__\" (lazy loading)\n",
-      " * Environment: production\n",
-      "\u001b[31m   WARNING: This is a development server. Do not use it in a production deployment.\u001b[0m\n",
-      "\u001b[2m   Use a production WSGI server instead.\u001b[0m\n",
-      " * Debug mode: off\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      " * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)\n",
-      "127.0.0.1 - - [13/Aug/2022 16:25:47] \"GET / HTTP/1.1\" 200 -\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "1.4\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "127.0.0.1 - - [13/Aug/2022 16:25:50] \"POST / HTTP/1.1\" 200 -\n"
-     ]
-    }
-   ],
-   "source": [
-    "if __name__==\"__main__\":\n",
-    "    app.run()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "012c973a",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "bbb49c23",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "bed03453",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask,render_template, request
+
+
+# In[2]:
+
+
+import joblib
+
+
+# In[3]:
+
+
+app= Flask(__name__)
+
+
+# In[4]:
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        rates=float(request.form.get("rates"))
+        print(rates)
+        model1=joblib.load("regression_DBS")
+        pred1=model1.predict([[rates]])
+        model2=joblib.load("tree_DBS")
+        pred2=model2.predict([[rates]])
+        return(render_template("index.html",result1=pred1,result2=pred2))
+    else:
+        return(render_template("index.html",result1="waiting",result2="waiting"))  
+
+
+# In[ ]:
+
+
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
